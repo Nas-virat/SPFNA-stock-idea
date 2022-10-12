79c5b329a29d7923044099575e105795625c0a01
KhChanon <chanon.619@hotmail.com>
2022-10-13 00:27:40 +0700

    modified button color in allidea page

--- client/src/pages/allidea/Allidea.tsx
+++ client/src/pages/allidea/Allidea.tsx
@@ -7,7 +7,6 @@ import Sidebar from '../../globalcomponents/Sidebar';
 
   // This function will scroll the window to the top 
 
-
 const Allidea = () => {
   const [showbutton, setShowbutton] = useState(false)
 
@@ -31,15 +30,15 @@ const Allidea = () => {
   return (
     <div>
       <Sidebar />
-      <div className='mx-3 w-4/5 ml-72'>
+      <div className='w-4/5 ml-72'>
         <p className='text-3xl font-bold mt-3'>Idea Space</p>
         <div className='flex flex-row justify-between h-12 my-4'>
-          <div className='flex flex-row border-solid border-8 h-full w-3/5 rounded-3xl mr-4'>
+          <div className='flex flex-row border-solid border-8 h-full w-4/5 rounded-3xl mr-4'>
             <img className='m-1 ml-2' src={Searchicon} alt='search icon'></img> 
             <input className='h-full w-4/5 rounded-3xl indent-11' placeholder='Search by Keyword' />
-            <button className='bg-[#8236FD] hover:bg-blue-700 text-white font-bold h-full w-1/5 rounded-3xl'>Search</button>
+            <button className='bg-[#856dab] hover:bg-[#4a366b] text-white font-bold h-full w-1/5 rounded-3xl'>Search</button>
           </div>
-          <button className='bg-[#8236FD] hover:bg-blue-700 text-white font-bold h-full w-1/6 rounded-3xl border-8 ml-3'>Create Post</button>
+          <button className='bg-[#856dab] hover:bg-[#4a366b] text-white font-bold h-full w-1/6 rounded-3xl border-8 ml-3'>Create Post</button>
         </div>
         <Ideapost/>
         <Ideapost/>
