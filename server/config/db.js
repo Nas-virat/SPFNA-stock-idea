32da544773f0427ccbdd061aeadf8851129294d5
Nas-virat <Napasvinitnantharat@gmail.com>
2022-10-26 14:30:35 +0700

    delete createIndex and findAndModify

--- server/config/db.js
+++ server/config/db.js
@@ -6,8 +6,6 @@ const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
         useUnifiedTopology: true,
         });
     
