f23f33d165a8c69b4473201c7b3de74d8e31a600
portunwa <portunwa@gmail.com>
2022-11-01 15:37:08 +0700

    Implement Login page, adjust register test case + change Swal parameters

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,27 +1,102 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
-import myphoto from '../../assets/SPFNAlogo.png'
-
-import Layout from '../../globalcomponents/Layout'
+import logo from '../../assets/SPFNAlogo.png'
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [data, setData] = useState({
+    userId: '',
+    password: '',
+  });
+
+  const handleSubmit = async () => {
+    if (data.userId === '' || data.password === '') {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Please fill in all the fields',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      })
+    } else {
+      try {
+        const res = await axios.post('http://localhost:5000/api/users/login', data);
+        if (res.data.success) {
+          Swal.fire({
+            title: 'Success!',
+            text: 'You have successfully logged in!',
+            icon: 'success',
+            timer: 1500,
+            timerProgressBar: true,
+            didOpen: () => {
+              Swal.showLoading()
+            },
+          }).then(() => {
+            navigate('/home');
+          })
+        } else {
+          Swal.fire({
+            title: 'Error!',
+            text: res.data.message,
+            icon: 'error',
+            confirmButtonText: 'OK'
+          })
+        }
+      } catch (err: any) {
+        Swal.fire({
+          icon: 'error',
+          title: 'Server Error Occured',
+          html: err.response.data,
+        })
+      }
+    }
+  };
+
   return (
     <body className = 'flex place-content-center h-screen bg-[#D6BBE8]'>
       <div className = 'self-center h-1/2 w-1/2 mx-auto flex items-center justify-center p-6 rounded-xl bg-white'>
-        <img className = 'mx-auto rounded-full pr-4 ' src={myphoto} alt='myphoto' width="300" height="300"></img>
+        <img className = 'mx-auto rounded-full pr-4 ' src={logo} alt='myphoto' width="300" height="300"></img>
         <div className='w-1/2'>
           <h1 className = 'text-3xl font-bold mb-6'>LOGIN</h1>
           <div className = 'form-group mb-6 ml-2'>
-            <label className = 'form-label inline-block font-medium text-2xl'>Username</label>
-            <input className = 'h-12 w-4/5 mt-3 rounded-3xl border-solid border-2 pl-3' placeholder = 'Username' />
+            <label className = 'form-label inline-block font-medium text-2xl'>Username or Email</label>
+            <input
+              id='userid-form'
+              className = 'h-12 w-4/5 mt-3 rounded-3xl border-solid border-2 pl-3'
+              placeholder = 'Username or Email'
+              onChange={(e) => setData({ ...data, userId: e.target.value })}
+              required
+            />
           </div>
           <div className = 'form-group mb-6 ml-2'>
               <label className = 'form-label inline-block font-medium text-2xl'>Password</label>
-              <input className = 'h-12 w-4/5 mt-3 rounded-3xl border-solid border-2 pl-3' placeholder = 'Password' />
+              <input
+                id='password-form'
+                className = 'h-12 w-4/5 mt-3 rounded-3xl border-solid border-2 pl-3'
+                placeholder = 'Password'
+                type='password'
+                onChange={(e) => setData({ ...data, password: e.target.value })}
+                required
+              />
           </div>
           <div className = 'flex flex-row justify-between w-4/5'>
-            <button className ='mt-3 ml-2 bg-[#856dab] hover:bg-[#4a366b] text-white font-medium h-9 w-32 rounded-3xl'>LOG IN</button>
-            <button className ='mt-3 ml-2 bg-[#9d8fb3] hover:bg-[#4a366b] text-white font-medium h-9 w-32 rounded-3xl'>REGISTER</button>
+            <button
+              id='btn-submit'
+              className='mt-3 ml-2 bg-[#856dab] hover:bg-[#4a366b] text-white font-medium h-9 w-32 rounded-3xl'
+              type='submit'
+              onClick = {() => handleSubmit()}
+            >
+              LOG IN
+            </button>
+            <button
+              id='btn-to-register'
+              className ='mt-3 ml-2 bg-[#9d8fb3] hover:bg-[#4a366b] text-white font-medium h-9 w-32 rounded-3xl'
+              onClick={() => navigate('/register')}
+            >
+              REGISTER
+            </button>
           </div>
         </div>
       </div>
--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -17,30 +17,44 @@ const Register = () => {
   });
 
   const handleSubmit = async () => {
-    try {
-      const res = await axios.post('http://localhost:5000/api/users/register', data);
-      if (res.data.success) {
+    if (data.username === '' || data.password === '' || data.email === '') {
+      Swal.fire({
+        title: 'Error!',
+        text: 'Please fill in all the fields',
+        icon: 'error',
+        confirmButtonText: 'OK'
+      })
+    } else {
+      try {
+        const res = await axios.post('http://localhost:5000/api/users/register', data);
+        if (res.data.success) {
+          Swal.fire({
+            title: 'Success!',
+            text: 'You have successfully registered!',
+            icon: 'success',
+            timer: 2000,
+            timerProgressBar: true,
+            didOpen: () => {
+              Swal.showLoading()
+            },
+          }).then(() => {
+            navigate('/home');
+          })
+        } else {
+          Swal.fire({
+            title: 'Error!',
+            text: res.data.message,
+            icon: 'error',
+            confirmButtonText: 'OK'
+          })
+        }
+      } catch (err: any) {
         Swal.fire({
-          title: 'Success!',
-          text: res.data.message,
-          icon: 'success',
-          confirmButtonText: 'OK'
-        })
-        navigate('/home');
-      } else {
-        Swal.fire({
-          title: 'Error!',
-          text: res.data.message,
           icon: 'error',
-          confirmButtonText: 'OK'
+          title: 'Server Error Occured',
+          html: err.response.data,
         })
       }
-    } catch (err: any) {
-      Swal.fire({
-        icon: 'error',
-        title: 'Error Occured',
-        html: err.response.data,
-      })
     }
   };
 
@@ -59,22 +73,28 @@ const Register = () => {
           <div className = 'form-group mb-4 pt-4 flex flex-col justify-between'>
             <label className = 'form-label inline-block font-medium text-2xl'>Username</label>
             <input
+              id='username-form'
               className = 'h-12 w-[50%] mt-3 rounded-3xl border-solid border-2 pl-3'
               placeholder = 'Username'
               onChange={(e) => setData({ ...data, username: e.target.value })}
+              required
             />
             <label className = 'form-label inline-block font-medium text-2xl pt-3'>Password</label>
             <input
+              id='password-form'
               className = 'h-12 w-[50%] mt-3 rounded-3xl border-solid border-2 pl-3'
               placeholder = 'Password'
               type='password'
               onChange={(e) => setData({ ...data, password: e.target.value })}
+              required
             />
             <label className = 'form-label inline-block font-medium text-2xl pt-3'>Email</label>
             <input
+              id='email-form'
               className = 'h-12 w-full mt-3 rounded-3xl border-solid border-2 pl-3'
               placeholder = 'Email'
-              onChange={(e) => setData({ ...data, email: e.target.value })}  
+              onChange={(e) => setData({ ...data, email: e.target.value })}
+              required
             />
           </div>
         </div>
@@ -84,15 +104,16 @@ const Register = () => {
           </div>
           <div className="flex pb-7 h-36">
             {profileImage.map((item : any) => (
-              <button key={item.id} className="flex flex-row items-center" type="button" onClick={() => setData({ ...data, image: item.alt })}>
+              <button id={`btn-img-${item.id}`} key={item.id} className="flex flex-row items-center" type="button" onClick={() => setData({ ...data, image: item.alt })}>
                 <img src={item.src} alt={item.alt} className={`${handleProfileImageStyle(item.alt)}`} />
               </button>
             ))}
           </div>
 
         <button
+          id='btn-submit'
           className ='bg-[#856dab] hover:bg-[#4a366b] text-white font-medium h-11 w-36 rounded-3xl mb-2'
-          type = 'submit'
+          type='submit'
           onClick = {() => handleSubmit()}
         >
           REGISTER
