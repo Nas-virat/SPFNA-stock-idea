aff2abb0f12a57bfb9c6039dc98e5988fdc9c9b7
Nas-virat <Napasvinitnantharat@gmail.com>
2022-11-01 17:31:19 +0700

    add cookieparser

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 const connectDB = require("./config/db");
 
 connectDB();
@@ -11,6 +12,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 const corsConfig = {
     origin: true,
