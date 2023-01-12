8f3c740bb5d97f8d9919a31fc5bd967170b0ab9a
Nas-virat <Napasvinitnantharat@gmail.com>
2023-01-12 20:35:50 +0700

    .onrender

--- server/utils/sendCookie.js
+++ server/utils/sendCookie.js
@@ -6,7 +6,7 @@ const sendCookie = (user = {}, statusCode, res) => {
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
         httpOnly: true,
-        domain: 'vercel.app'
+        domain: '.onrender.com'
     }
 
     res.status(statusCode).cookie('token', token, options).json({
