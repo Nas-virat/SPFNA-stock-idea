196929c9b71e49814fb74d2d4d913e62bd7f5a5a
Nas-virat <Napasvinitnantharat@gmail.com>
2023-01-12 20:01:13 +0700

    cookie adjust

--- server/utils/sendCookie.js
+++ server/utils/sendCookie.js
@@ -5,7 +5,6 @@ const sendCookie = (user = {}, statusCode, res) => {
         expires: new Date(
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
-        httpOnly: true
     }
 
     res.status(statusCode).cookie('token', token, options).json({
